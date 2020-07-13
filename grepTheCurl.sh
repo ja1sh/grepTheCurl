4994650ea1ef4a6538a0f0cfe4065d13247ccae0
Jai Kumar Sharma <jaisharmafps@gmail.com>
2020-07-13 16:26:39 +0530

    Update grepTheCurl.sh

--- grepTheCurl.sh
+++ grepTheCurl.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-echo 'Welcome to grepTheCurl'
+line="$1"
+echo 'Welcome to grepTheCurl: Hakrawling on' $1
+hakrawler -url $line -plain -depth 3 | tee -a /urls.txt
+sed 's/^/url=/;' /urls.txt | tee -a /curls.txt
+curl -sS -K /curls.txt | grep -Eoi "REGEX"
