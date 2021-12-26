afe9d60766f5f9465f005bb5165a74ce0c7e548b
darker <s4nixd@gmail.com>
2021-12-26 14:08:30 +0100

    check: check hakrawler is installed before run it

--- grepTheCurl.sh
+++ grepTheCurl.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+
 line="$1"
 echo 'Welcome to grepTheCurl: Hakrawling on' $1
+
+# Just to check if hakrawler is installed or not
+if ! command -v hakrawler &> /dev/null
+then
+    echo "[x] Missing requirements..."
+    echo "[x] hakrawler could not be found."
+    echo "[x] please check this to install : https://github.com/hakluke/hakrawler"
+    exit
+fi
+
 hakrawler -url $line -plain -depth 3 | tee -a /urls.txt
 sed 's/^/url=/;' /urls.txt | tee -a /curls.txt
 curl -sS -K /curls.txt | grep -Eoi "REGEX"
